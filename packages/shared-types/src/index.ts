cf32d1f4426a2c352c096d068f7ab3745fe30535
Kim, Hyeonseo <dodok8@gmail.com>
2025-02-13 20:56:47 +0900

    ✨ feat: shared-types package

---
+++ packages/shared-types/src/index.ts
@@ -0,0 +1,16 @@
+export interface Song {
+  id: string;
+  artist: string;
+  releaseVersion: string;
+  chapter: string;
+  title_localized: Record<string, string>;
+  source_localized?: {
+    default: string;
+  };
+  charts: {
+    difficultyLevel: string;
+    difficultyDecimal: number;
+    chartDesigner: string;
+    jacketDesigner: any;
+  }[];
+}
