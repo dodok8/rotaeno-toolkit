606d02ecbeb0ab62f8d274192362aae07666bc72
Kim, Hyeonseo <dodok8@gmail.com>
2025-02-16 21:53:28 +0900

    🏷️ add: Score interface

--- packages/shared-types/src/index.ts
+++ packages/shared-types/src/index.ts
@@ -15,3 +15,23 @@ export interface Song {
     jacketDesigner: any;
   }[];
 }
+
+export interface Score {
+  id: string;
+  imageUrl: string;
+  artist: string;
+  releaseVersion: string;
+  chapter: string;
+  title_localized: Record<string, string>;
+  source_localized?: {
+    default: string;
+  };
+  charts: {
+    difficultyLevel: string;
+    difficultyDecimal: number;
+    chartDesigner: string;
+    jacketDesigner: any;
+    score: number;
+    rating: number;
+  }[];
+}
